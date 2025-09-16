2eaf3127ad3cd53f4cc2f2dbd3acacf3f501f5b1
Farhan <far20shid@gmail.com>
2025-09-16 12:27:36 +0100

    init

---
+++ REPLAY JUICE IT PLATFORMER/Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+
+    public Transform t;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
